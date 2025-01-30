d242d390993e432fed9a1e0366db205217964606
Vynzter <vynraven@gmail.com>
2025-01-30 18:54:06 +0700

    update html, css, js BMI basic

--- BMI-Calculator/JS/script.js
+++ BMI-Calculator/JS/script.js
@@ -1,1 +1,22 @@
 //BMI Calculator Javascript//
+
+function validateForm() {
+    let inputBodyWeight = document.getElementById('body-weight-input').value;
+    let inputAge = document.getElementById('age-input').value;
+    let inputBodyHeight = document.getElementById('body-height-input').value;
+
+    console.log ('Body Weight:' + inputBodyWeight);
+    console.log ('Age:' + inputAge);
+    console.log ('Body Height:' + inputBodyHeight);
+
+    if (inputBodyWeight !='' && inputAge !='' && inputBodyHeight !='') {
+        let calculate = parseInt(inputBodyWeight) + parseInt(inputBodyHeight);
+        updateResult(calculate);
+    } else {
+        alert('Your input is empty, Please Check Again!');
+        }
+    }
+
+    function updateResult(value) {
+        document.getElementById('result-calculation').innerHTML = value;
+    }
